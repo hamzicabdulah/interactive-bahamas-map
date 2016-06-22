5f150bee6d692afa13d6b64462a18dbcf8361530
thinsoldier <thinsoldier@thinsoldier.com>
2016-06-22 08:16:02 -0600

    Map now uses multiple boxes within each island region (where necessary) so that the zoomed in view can omit parts of other islands.

---
+++ islands-cooked.js
@@ -0,0 +1,246 @@
+var islands = 
+[
+    {
+        "name": "Grand Bahama",
+        "boundingbox": {
+            "x": "0",
+            "y": "31",
+            "w": "126",
+            "h": "42",
+            "style": {}
+        },
+        "subsections": []
+    },
+    {
+        "name": "Abaco",
+        "boundingbox": {
+            "x": "104",
+            "y": "8",
+            "w": "106",
+            "h": "133",
+            "style": {}
+        },
+        "subsections": [
+            {
+                "x": 104,
+                "y": 8,
+                "w": 106,
+                "h": 29,
+                "style": ""
+            },
+            {
+                "x": 141,
+                "y": 37,
+                "w": 69,
+                "h": 104,
+                "style": ""
+            }
+        ]
+    },
+    {
+        "name": "Andros",
+        "boundingbox": {
+            "x": "49",
+            "y": "192",
+            "w": "105",
+            "h": "177",
+            "style": {
+                "left": "49px",
+                "top": "192px",
+                "width": "105px",
+                "height": "177px"
+            }
+        },
+        "subsections": [
+            {
+                "x": 49,
+                "y": 192,
+                "w": 90,
+                "h": 101,
+                "style": ""
+            },
+            {
+                "x": 49,
+                "y": 293,
+                "w": 105,
+                "h": 76,
+                "style": ""
+            }
+        ]
+    },
+    {
+        "name": "New Providence",
+        "boundingbox": {
+            "x": "140",
+            "y": "208",
+            "w": "58",
+            "h": "28",
+            "style": {
+                "left": "140px",
+                "top": "208px",
+                "width": "58px",
+                "height": "28px"
+            }
+        },
+        "subsections": []
+    },
+    {
+        "name": "Eleuthera",
+        "boundingbox": {
+            "x": "212",
+            "y": "160",
+            "w": "90",
+            "h": "112",
+            "style": {
+                "left": "212px",
+                "top": "160px",
+                "width": "90px",
+                "height": "112px"
+            }
+        },
+        "subsections": [
+            {
+                "x": 212,
+                "y": 160,
+                "w": 90,
+                "h": 81,
+                "style": ""
+            },
+            {
+                "x": 266,
+                "y": 241,
+                "w": 36,
+                "h": 31,
+                "style": ""
+            }
+        ]
+    },
+    {
+        "name": "Cat Island",
+        "boundingbox": {
+            "x": "322",
+            "y": "249",
+            "w": "63",
+            "h": "78",
+            "style": {
+                "left": "322px",
+                "top": "249px",
+                "width": "63px",
+                "height": "78px"
+            }
+        },
+        "subsections": []
+    },
+    {
+        "name": "San Salvador",
+        "boundingbox": {
+            "x": "440",
+            "y": "311",
+            "w": "31",
+            "h": "34",
+            "style": {
+                "left": "440px",
+                "top": "311px",
+                "width": "31px",
+                "height": "34px"
+            }
+        },
+        "subsections": []
+    },
+    {
+        "name": "Long Island",
+        "boundingbox": {
+            "x": "365",
+            "y": "360",
+            "w": "65",
+            "h": "99",
+            "style": {
+                "left": "365px",
+                "top": "360px",
+                "width": "65px",
+                "height": "99px"
+            }
+        },
+        "subsections": [
+            {
+                "x": 365,
+                "y": 360,
+                "w": 35,
+                "h": 99,
+                "style": ""
+            },
+            {
+                "x": 400,
+                "y": 422,
+                "w": 30,
+                "h": 37,
+                "style": ""
+            }
+        ]
+    },
+    {
+        "name": "Rum Cay",
+        "boundingbox": {
+            "x": "402",
+            "y": "355",
+            "w": "33",
+            "h": "24",
+            "style": {
+                "left": "402px",
+                "top": "355px",
+                "width": "33px",
+                "height": "24px"
+            }
+        },
+        "subsections": []
+    },
+    {
+        "name": "Crooked Island",
+        "boundingbox": {
+            "x": "456",
+            "y": "446",
+            "w": "73",
+            "h": "90",
+            "style": {
+                "left": "456px",
+                "top": "446px",
+                "width": "73px",
+                "height": "90px"
+            }
+        },
+        "subsections": []
+    },
+    {
+        "name": "Mayaguana",
+        "boundingbox": {
+            "x": "578",
+            "y": "487",
+            "w": "63",
+            "h": "35",
+            "style": {
+                "left": "578px",
+                "top": "487px",
+                "width": "63px",
+                "height": "35px"
+            }
+        },
+        "subsections": []
+    },
+    {
+        "name": "Inagua",
+        "boundingbox": {
+            "x": "526",
+            "y": "587",
+            "w": "97",
+            "h": "79",
+            "style": {
+                "left": "526px",
+                "top": "587px",
+                "width": "97px",
+                "height": "79px"
+            }
+        },
+        "subsections": []
+    }
+]
+;
