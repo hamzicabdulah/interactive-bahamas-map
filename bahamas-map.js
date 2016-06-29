fd094a5f648b6ee1b3a254d53de748cb45a4df31
thinsoldier <thinsoldier@thinsoldier.com>
2016-06-29 00:57:23 -0600

    Change how hovering works so that the mouse can move over to the info box and click without the info box disappearing.

--- bahamas-map.js
+++ bahamas-map.js
@@ -99,7 +99,7 @@ for(var i=0; i<islands.length; i++)
 }
 
 // Observe sub-regions to apply hover class to their parent bounding box when they are hovered.
-$('.map-sub, .map-island span').hover( function(e){
+$('.map-sub, .map-island span, .map-island-info').hover( function(e){
 		$(e.target).parent().toggleClass('hovered');
 		$('.i-bahamasmap').toggleClass('hovermode');
 	});
