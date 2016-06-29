b1d6491a186bc21ca4b6efd04e777fd44aba25d1
thinsoldier <thinsoldier@thinsoldier.com>
2016-06-29 00:06:57 -0600

    Add js code that moves the hard coded info markup into the island hover divs and watches for clicks.

--- bahamas-map.js
+++ bahamas-map.js
@@ -33,7 +33,25 @@ function xywhToStyle( obj )
 	return { width:obj.w, height:obj.h, top:obj.y+"px", left:obj.x+"px" };
 }
 
+function tokenizeName( text )
+{
+	return text.toLowerCase().replace(' ','_');
+}
+
 var mapdiv = $('.i-bahamasmap');
+var infoSections = $('.i-bahamasmap section');
+
+// Collect island info section elements into an object keyed
+// by the island names in a tokenized form.
+var infoData = {};
+
+infoSections.each( function(i,e){
+	var name = $(e).find('header a').text();
+	var nameToken = tokenizeName( name );
+	infoData[ nameToken ] = e;
+});
+
+//console.log(infoData);
 
 for(var i=0; i<islands.length; i++)
 {
@@ -62,6 +80,17 @@ for(var i=0; i<islands.length; i++)
 		var many = makeManySubRegions( current );
 		regionDiv.append( many );
 	}
+	
+	// Build info boxes from section elements in markup.
+	// Find appropriate section by matching island name 
+	// in js data to text in section header.
+	var nameToken = tokenizeName( current.name );
+
+	if( infoData[ nameToken ] )
+	{
+		regionDiv.append( infoData[ nameToken ] );
+	} else { console.warn( 'No Island info section match found for '+current.name ); }
+	
 }
 
 // Observe sub-regions to apply hover class to their parent bounding box when they are hovered.
@@ -69,3 +98,10 @@ $('.map-sub, .map-island span').hover( function(e){
 		$(e.target).parent().toggleClass('hovered').css('background','transparent');
 		$('.i-bahamasmap').toggleClass('hovermode');
 	});
+
+// Observer island regions to go to the url in their info box if anywere within the region is clicked.
+$('.map-island').click( function(e){
+	e.preventDefault();
+	var href = $(e.target).closest('.map-island').find('a').attr('href');
+	window.location.href = href;
+});
